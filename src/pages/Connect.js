10d92be4d73428dda6d475b1c0c90eb04b1e9ca5
Vaani <vaanipathariya@gmail.com>
2023-10-25 16:32:15 +0530

    Adding links to social media

--- src/pages/Connect.js
+++ src/pages/Connect.js
@@ -16,8 +16,8 @@ const Connect=()=>{
             <div className="flex images">
             <Link to="https://github.com/Vaani-pathariya"><img src={github} className="image_di"></img></Link>
             <Link to="https://www.linkedin.com/in/vaani-pathariya/"><img src={linkedin} className="image_di"></img></Link>
-            <Link><img src={leetcode} className="image_di"></img></Link>
-            <Link><img src={twitter} className="image_di"></img></Link>
+            <Link to="https://leetcode.com/quantumm_/"><img src={leetcode} className="image_di"></img></Link>
+            <Link to="https://twitter.com/PathariyaVaani"><img src={twitter} className="image_di"></img></Link>
             </div>
         </div>
     )
