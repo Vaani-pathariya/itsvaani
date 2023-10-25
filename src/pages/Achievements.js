e8dd857b88375e1b4ff5bd1c5fe7b625f406a774
Vaani <vaanipathariya@gmail.com>
2023-10-25 22:46:16 +0530

    added Achievements

--- src/pages/Achievements.js
+++ src/pages/Achievements.js
@@ -13,28 +13,130 @@ const Achievement=()=>{
                 <div className="time">
                     <div>
                     <ol style={{ listStyleType: 'circle' }}>
-                    <li className="below-text-black new-font bold">Hello</li>
+                    <li className="below-text-black new-font bold">Got selected in top 67 team out of 20,000 registrations in ICC | NIUM Next in 2.0 Global Cricket Hackathon</li>
                     </ol>
                     <hr></hr>
                     </div>
                     <div>
                     <ol style={{ listStyleType: 'circle' }}>
-                    <li className="below-text-black new-font bold">Hello</li>
+                    <li className="below-text-black new-font bold">Got promoted to Alpha Microsoft Learn Student Ambassador</li>
+                    </ol>
+                    <hr></hr>
+                    </div>
+                    <div>
+                    <ol style={{ listStyleType: 'circle' }}>
+                    <li className="below-text-black new-font bold">Got accepted as Microsoft Learn Student Ambassador</li>
+                    </ol>
+                    <hr></hr>
+                    </div>
+                    <div>
+                    <ol style={{ listStyleType: 'circle' }}>
+                    <li className="below-text-black new-font bold">Maintainer of Hacktoberfest repository 'Foss Events 2.0' under GDSC JSSATEN organisation</li>
+                    </ol>
+                    <hr></hr>
+                    </div>
+                    <div>
+                    <ol style={{ listStyleType: 'circle' }}>
+                    <li className="below-text-black new-font bold">Gave a session on 'Chat GPT and creating your own LLM bot using Embedchains' at 'Explorations in AI' event held by GDSC JSSATEN in collaboration with GDG Gurugram </li>
+                    </ol>
+                    <hr></hr>
+                    </div>
+                    <div>
+                    <ol style={{ listStyleType: 'circle' }}>
+                    <li className="below-text-black new-font bold">Got accepted as a Web Developer at GDSC JSSATEN</li>
+                    </ol>
+                    <hr></hr>
+                    </div>
+                    <div>
+                    <ol style={{ listStyleType: 'circle' }}>
+                    <li className="below-text-black new-font bold">Got featured by Samsung India on Twitter for Samsung solve for Tomorrow Hackathon</li>
+                    </ol>
+                    <hr></hr>
+                    </div>
+                    <div>
+                    <ol style={{ listStyleType: 'circle' }}>
+                    <li className="below-text-black new-font bold">My team 'Tridev' got selected as Top 30 winner nation wide in 'Samsung Solve for Tomorrow Season2'</li>
+                    </ol>
+                    <hr></hr>
+                    </div>
+                    <div>
+                    <ol style={{ listStyleType: 'circle' }}>
+                    <li className="below-text-black new-font bold">GSSOC '23 contributor (May- August)</li>
+                    </ol>
+                    <hr></hr>
+                    </div>
+                    <div>
+                    <ol style={{ listStyleType: 'circle' }}>
+                    <li className="below-text-black new-font bold">Cleared the first round of Google Girl Hackathon</li>
+                    </ol>
+                    <hr></hr>
+                    </div>
+                    <div>
+                    <ol style={{ listStyleType: 'circle' }}>
+                    <li className="below-text-black new-font bold">Submited a proposal for Code for GovTech (C4GT)</li>
+                    </ol>
+                    <hr></hr>
+                    </div>
+                    <div>
+                    <ol style={{ listStyleType: 'circle' }}>
+                    <li className="below-text-black new-font bold">Won 2nd runner up at Trident Hacks'23 Nationals</li>
+                    </ol>
+                    <hr></hr>
+                    </div>
+                    <div>
+                    <ol style={{ listStyleType: 'circle' }}>
+                    <li className="below-text-black new-font bold">Participated in MHACKS 15, a Hackathon by University of Michigan</li>
                     </ol>
                     <hr></hr>
                     </div>
                 </div>
-                <div className="simple-black">YEAR 2023</div>
+                <div className="simple-black">YEAR 2022</div>
                 <div className="time">
-                    <div>
+                <div>
                     <ol style={{ listStyleType: 'circle' }}>
-                    <li className="below-text-black new-font bold">Hello</li>
+                    <li className="below-text-black new-font bold">Participated in Google Solution Challenge</li>
                     </ol>
                     <hr></hr>
                     </div>
                     <div>
                     <ol style={{ listStyleType: 'circle' }}>
-                    <li className="below-text-black new-font bold">Hello</li>
+                    <li className="below-text-black new-font bold">Participated in MLH Global Hack Week</li>
+                    </ol>
+                    <hr></hr>
+                    </div>
+                    <div>
+                    <ol style={{ listStyleType: 'circle' }}>
+                    <li className="below-text-black new-font bold">Got 5 pull requests merged on the first day of Hacktoberfest and received a swag kit.</li>
+                    </ol>
+                    <hr></hr>
+                    </div>
+                    <div>
+                    <ol style={{ listStyleType: 'circle' }}>
+                    <li className="below-text-black new-font bold">Participated in Google Kickstart round F and secured 5369 rank</li>
+                    </ol>
+                    <hr></hr>
+                    </div>
+                    <div>
+                    <ol style={{ listStyleType: 'circle' }}>
+                    <li className="below-text-black new-font bold">Got accepted as a Technical Member in EDC JSSATEN (Entrepreneurship Development Cell)</li>
+                    </ol>
+                    <hr></hr>
+                    </div>
+                </div>
+                <div className="simple-black">YEAR 2021</div>
+                <div className="time">
+                    <div>
+                    <ol style={{ listStyleType: 'circle' }}>
+                    <li className="below-text-black new-font bold">Got accepted as a Member at Impetus Student society at my Institute </li>
+                    </ol>
+                    <hr></hr>
+                    </div>
+                </div>
+                <div className="simple-black">YEAR 2020</div>
+                <div className="time">
+                    <div>
+                    <ol style={{ listStyleType: 'circle' }}>
+                    <li className="below-text-black new-font bold">Participated in Google Technovation Idea Lab 2020</li>
                     </ol>
                     <hr></hr>
                     </div>
