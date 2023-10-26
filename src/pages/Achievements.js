f3f59697bec60737a39e6492eefcee3229aed528
Vaani <vaanipathariya@gmail.com>
2023-10-26 21:34:35 +0530

    Achievements

--- src/pages/Achievements.js
+++ src/pages/Achievements.js
@@ -5,7 +5,7 @@ const Achievement=()=>{
     return (
         <div className="achieve">
             <div className="sizing">
-                <div className="changed-font-black siz">Defining Excellence One Achievement at a Time</div>
+                <div className="changed-font-black siz">Defining Excellence One<br></br>Achievement at a Time</div>
                 <div className="below-text-black new-font">With each Achievement as well as failure, I try to improve myself as much as possible.</div>
             </div>
             <div>
